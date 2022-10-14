916d78a9e32edeff05e965869b1434f5c7866c90
u0026939 <j.cordry@tees.ac.uk>
2022-10-14 13:19:44 +0100

    First commit

---
+++ app/src/main/java/uk/ac/tees/mad/u0026939/gitdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.mad.u0026939.gitdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
